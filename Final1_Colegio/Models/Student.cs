f70e78bdfeb1cfee27c1d83b6ed7ae5ab22ca3f0
Wilbor <wilborma2009@hotmail.com>
2017-12-01 00:32:41 -0500

    Agregar archivos de proyecto.

---
+++ Final1_Colegio/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Final1_Colegio.Models
+{
+    public class Student
+    {
+        [Key]
+        public int IdStudent { get; set; }
+        public string Full_Name { get; set; }
+        public int DNI { get; set; }
+    }
+}
